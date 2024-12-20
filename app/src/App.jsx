bc84ea95629d6d9a2aa7c96e30ad03830f677fc9
Giuseppe Esposito <espositogiuseppe2017@gmai.com>
2024-12-20 16:24:06 +0100

    feat:introdotte le pagine funzionanti di caricamento e visualizzazione articoli formativi

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -5,6 +5,8 @@ import Home from './components/pages/Homepage';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import LoginPage from './components/pages/LoginPage';
 import { MentorSearchForm } from './components/pages/mentor-search-form';
+import FileHomePage from './components/pages/FileHomePage';
+import FileAddDocument from './components/pages/fileAddDocument';
 
 
 function App() {
@@ -17,6 +19,8 @@ function App() {
         <Route path="/register" element={<RegistrationPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/mentorsearch" element={<MentorSearchForm />} />
+        <Route path="/filepage" element={<FileHomePage />} />
+        <Route path="/addfile" element={<FileAddDocument />} />
       </Routes>
     </BrowserRouter>
   );
--- app/src/firebase/firebase.js
+++ app/src/firebase/firebase.js
@@ -1,6 +1,7 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp } from "firebase/app"; 
+import { getStorage, connectStorageEmulator } from "firebase/storage";
 
-
+// La tua configurazione Firebase
 const firebaseConfig = {
     apiKey: import.meta.env.VITE_API_KEY,
     authDomain: import.meta.env.VITE_AUTH_DOMAIN,
@@ -11,7 +12,15 @@ const firebaseConfig = {
     measurementId: import.meta.env.VITE_MEASUREMENT_ID
 };
 
-
-
+// Inizializza Firebase
 const app = initializeApp(firebaseConfig);
+
+// Aggiungi la configurazione per l'emulatore di Firebase Storage, se siamo in localhost
+const storage = getStorage(app);
+if (window.location.hostname === "localhost") {
+    // Connetti l'emulatore di Firebase Storage alla porta 9199
+    connectStorageEmulator(storage, "localhost", 9199);
+}
+
+// Esporta l'app come export default
 export default app;
